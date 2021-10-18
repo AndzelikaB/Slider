b985ddbfd7d5f42c32d4a7f57eb6fba6dc2810a3
Andzelika Botwina <Andzelika.botwina@gmail.com>
2021-10-18 18:22:11 +0200

    working on slider

--- js/script.js
+++ js/script.js
@@ -2,26 +2,23 @@ const slideNumber = document.querySelector(".slideNumber");
 let numberOfSlides = document.querySelector(".totalSlides");
 const previewButton = document.querySelector(".previewButton");
 const nextButton = document.querySelector(".nextButton");
-var slide = document.querySelectorAll(".slide");
+let slide = document.querySelectorAll(".slide");
 let countSlides = 0;
 var slideCount = slide.length - 1;
+numberOfSlides.innerHTML = slideCount + 1;
+numberOfSlides = slideCount;;
 
-numberOfSlides = slideCount;
 function changeSlide() {
     Array.from(slide).forEach((el) => el.classList.remove('active'));
 
-    let zmienna = slide[countSlides];
-    zmienna.classList.add("active");
-
+    let cSlides = slide[countSlides];
+    cSlides.classList.add("active");
     Array.from(slide).forEach(function (el) {
         el.classList.add('d-none');
     });
-
-    zmienna.classList.remove("d-none");
-
-    console.log("countSlides change slide" + countSlides);
+    cSlides.classList.remove("d-none");
 }
-// Kocham Andzie
+
 function PreviewSlide() {
     countSlides--;
     if (countSlides == 0) {
@@ -44,7 +41,8 @@ function NextSlide() {
     slideNumber.innerHTML = countSlides + 1;
     changeSlide()
 }
-// Nasłuchiwanie
+
+// NS
 previewButton.addEventListener("click", PreviewSlide);
 nextButton.addEventListener("click", NextSlide);
 
